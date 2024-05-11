a22f9dc03745740ebc6fcc505aa9d9de4fda64f4
peterwup <peter.holschbach@gmail.com>
2024-05-11 15:21:29 +0200

    Update start-PBS.sh

--- start-PBS.sh
+++ start-PBS.sh
@@ -7,12 +7,12 @@
 storeid=backup                                                  # Variable - um welches Storage handelt es sich
 mac="00:08:9B:CC:DD:02"                                         # MAC Adresse des PBS-Servers
 host="192.168.0.23"                                             # Hostname oder IP des PBS-Servers
-remoteuser="admin"                                                                                      # username @ host / for qnap = admin
+remoteuser="admin"                                              # username @ host / for qnap = admin
 nic=enp2s0                                                      # Netzwerk-Karte über die WOL läuft
-serverTimeout=300                                                                                               # maximum time till we assume that the server will not respond in seconds
-storagetype=dir                                                                                                 # type of the storage "dir" or "" ... 
+serverTimeout=30                                                # maximum time till we assume that the server will not respond in seconds
+storagetype=cifs                                                # type of the storage "dir" or "cifs" ... 
 sleep_after_wakeup=8m                                           # Zeit die nach dem wakeup des servers gewartet wird bevor es weiter gehen soll
-                                                                                                                                # mein QNAS brauch locker 5 Minuten bevor es nach dem start erreichbar ist
+                                                                # mein QNAS brauch locker 5 Minuten bevor es nach dem start erreichbar ist
 
 #----------------------- Programmablauf ---------------------------------------------------------------------------#
 
@@ -20,30 +20,31 @@ sleep_after_wakeup=8m                                           # Zeit die nach 
 #
 # Check if we can reach the server by ping and if the backup storage is mounted
 # Returns:
-#               2 = down = server is not reachable by ping
+#       2 = down = server is not reachable by ping
 #       1 = inactive = server is reachable by ping but backup storage is not mounted
 #       0 = active = server is up and storeg is mounted
 is_server_up () {
     # cheks if the server is reachable and if the backup storage is mounted
-        if ping -c 1 $host &> /dev/null; then                           # prüfe, ob der Backup-Server schon läuft
+    if ping -c 1 $host &> /dev/null; then              # prüfe, ob der Backup-Server schon läuft
         echo "Der Backupserver läuft bereits"
-                com=$(pvesm status --storage $storeid | tail -1);
+        # check if storage is up
+        com=$(pvesm status --storage $storeid | tail -1);
         [[ "$com" =~ ($storeid+ +)($storagetype+ +)(active|disabled) ]];
-                if [ "${BASH_REMATCH[3]}" == "active" ]; then  
-                  return 0
-                else
-                  return 1
-                fi
+        if [ "${BASH_REMATCH[3]}" == "active" ]; then  
+            return 0
+            else
+              return 1
+            fi
     else
-                return 2
-        fi
+        return 2
+    fi
 }
 
 #################################
 
 is_server_up
 if [[ "$?" != 0 ]]; then
-        # start the server by WOL
+    # start the server by WOL
     /usr/sbin/etherwake -i $nic $mac     
     # wait till the server will be up
     sleep $sleep_after_wakeup
