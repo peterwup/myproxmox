db41be78f1863b47d2f7641c18893316df92d5bf
peterwup <peter.holschbach@gmail.com>
2024-05-15 18:51:18 +0200

    Update getscripts.sh

--- getscripts.sh
+++ getscripts.sh
@@ -4,10 +4,12 @@ wget https://raw.githubusercontent.com/peterwup/myproxmox/main/start-PBS.sh  -O 
 wget https://raw.githubusercontent.com/peterwup/myproxmox/main/stop-PBS.sh -O ~/stop-PBS.sh --backups=0
 wget https://raw.githubusercontent.com/peterwup/myproxmox/main/vzdump-hook-script -O /usr/local/bin/vzdump-hook-script --backups=0
 wget https://raw.githubusercontent.com/peterwup/myproxmox/main/prox_config_backup.sh -O ~/prox_config_backup.sh --backups=0
-wget https://raw.githubusercontent.com/peterwup/myproxmox/main/cleanup.sh -O ~/cleanup.sh --backups=0
+wget https://raw.githubusercontent.com/peterwup/myproxmox/main/cleanup.sh -O ~/cleanup.sh --backups=0 -O /usr/local/bin/vzdump-hook-script-with --backups=0
+wget https://raw.githubusercontent.com/peterwup/myproxmox/main/vzdump-hook-script-with 
 #
 # mv vzdump-hook-script /usr/local/bin/vzdump-hook-script 
 chmod a+x /usr/local/bin/vzdump-hook-script
+chmod a+x /usr/local/bin/vzdump-hook-script-with
 #
 chmod a+x stop-PBS.sh
 chmod a+x start-PBS.sh
