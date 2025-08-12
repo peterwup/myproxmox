29662b18e19e038ea4fb549486cb7dd6f7de15b6
peterwup <19516460+peterwup@users.noreply.github.com>
2025-08-12 09:35:46 +0200

    Update getscripts.sh
    trilium hook script fehlte noch

--- getscripts.sh
+++ getscripts.sh
@@ -9,12 +9,14 @@ wget https://raw.githubusercontent.com/peterwup/myproxmox/main/cleanup.sh -O ~/c
 wget https://raw.githubusercontent.com/peterwup/myproxmox/main/vzdump-hook-script-with --backups=0 -O /usr/local/bin/vzdump-hook-script-with
 # chek if snipet folder exist and create if not
 wget https://raw.githubusercontent.com/peterwup/myproxmox/main/paperless-hook-script --backups=0 -O /var/lib/vz/snippets/paperless-hook-script
+wget https://raw.githubusercontent.com/peterwup/myproxmox/main/trilium-hook-script --backups=0 -O /var/lib/vz/snippets/trilium-hook-script
 #
 # mv vzdump-hook-script /usr/local/bin/vzdump-hook-script 
 chmod a+x /usr/local/bin/vzdump-hook-script
 chmod a+x /usr/local/bin/vzdump-hook-script-with
 # pct set 109 --hookscript local:snippets/paperless-hook-script to add hook script to CT
 chmod a+x /var/lib/vz/snippets/paperless-hook-script
+chmod a+x /var/lib/vz/snippets/trilium-hook-script
 #
 chmod a+x stop-PBS.sh
 chmod a+x start-PBS.sh
